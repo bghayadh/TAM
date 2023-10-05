a07d2fb49b44340fc210b87576f536dc7091e326
FatimahFarasha <FatimahFarasha@gmail.com>
2023-10-05 21:54:38 +0300

    CHANGES

--- src/main/java/com/aliat/alm/telkom/Parser/LoadFileDWDMHuawei.java
+++ src/main/java/com/aliat/alm/telkom/Parser/LoadFileDWDMHuawei.java
@@ -316,7 +316,7 @@ public class LoadFileDWDMHuawei {
 								  ResultSet rsinit3 = stmtp1.executeQuery(sqlStmtinit3);
 								  while(rsinit3.next()) {
 									  wareID=rsinit3.getString("WARE_ID");
-									  wareName = rsinit3.getString("WARE_ID");
+									  wareName = rsinit3.getString("WARE_NAME");
 									  longi=rsinit3.getString("LONGITUDE");
 									  lat = rsinit3.getString("LATITUDE");
 								  }
