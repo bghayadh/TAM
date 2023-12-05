f238a78b209316b78e1def8419cf46b6a46db1e8
zeina <zeina@DESKTOP-OIU0ANF>
2023-12-06 00:38:01 +0200

    doneee

--- src/main/java/com/aliat/alm/controller/SystemSettingsController.java
+++ src/main/java/com/aliat/alm/controller/SystemSettingsController.java
@@ -2,22 +2,44 @@ package com.aliat.alm.controller;
 
 import java.util.Locale;
 
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.aliat.alm.common.AlmDbSession;
+import com.aliat.alm.common.Notify;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @Controller
 public class SystemSettingsController {
-
+	private Session session = null;
 	
+	@Autowired
+	Notify notification;
 	@RequestMapping(value = "/systemSettings", method = RequestMethod.GET)
 	public String antennaDetails(Locale locale, Model model) {
 		//logger.info("Welcome home! The client locale is {}.", locale);
+		session = AlmDbSession.getInstance().getSession();
 
-		
-			return "systemSettings";
+		if (session != null && session.isOpen()) {
+			try {
+			
+			notification.headerNotifications(session, model);
+	     
+			}catch(Exception e){
+				System.out.println("Error in rolePermission"+e.toString());
+			}
+			finally {
+				if (session != null && session.isOpen()) {
+					session.close();
+				}
+			}
+
+		}
+				
+	return "systemSettings";
 	}
-	
-	
 }
