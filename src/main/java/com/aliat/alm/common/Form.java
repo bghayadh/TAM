a8e20de93c9b75b46d043e5cbe9b1267e5c72111
Zeina <zeinadarwish44@gmail.com>
2025-07-22 23:35:34 +0300

    ggg

--- src/main/java/com/aliat/alm/common/Form.java
+++ src/main/java/com/aliat/alm/common/Form.java
@@ -87,6 +87,103 @@ public class Form {
 	//return "header";
 
 	}
+	
+	///// DN pending nav //////////////
+	
+	@SuppressWarnings("rawtypes")
+	@RequestMapping(value = "/dnNavigation", method = RequestMethod.GET)
+	public String[] dnNavigation(Session session, String queryBase, String moduleIDName, String moduleIDValue, String moduleLMD, String navAction) throws ParseException {
+	    String[] result = new String[3];
+	    int currentIndex;
+	    int lastIndex;
+	    Query q;
+	    JSONObject rowNav;
+	    JSONParser jsonParser = new JSONParser();
+
+	    // Step 1: Get total count and current row number
+	    String countAndRowQuery =
+	            "SELECT json_object(" +
+	            "'CountAll' VALUE (SELECT COUNT(*) FROM (" + queryBase + ")), " +
+	            "'CurrentRowNum' VALUE (SELECT row_num FROM (SELECT " + moduleIDName +
+	            ", ROW_NUMBER() OVER (ORDER BY " + moduleLMD + " DESC, " + moduleIDName + " DESC) AS row_num FROM (" + queryBase + ")) " +
+	            "WHERE " + moduleIDName + " = :param1)) FROM dual";
+
+	    q = session.createNativeQuery(countAndRowQuery);
+	    q.setParameter("param1", moduleIDValue);
+	    rowNav = (JSONObject) jsonParser.parse(q.getSingleResult().toString());
+
+	    result[0] = rowNav.get("CountAll").toString();
+	    result[1] = rowNav.get("CurrentRowNum").toString();
+
+	    lastIndex = Integer.parseInt(result[0]);
+	    currentIndex = Integer.parseInt(result[1]);
+
+	    switch (navAction) {
+	        case "1": // Next
+	            if (currentIndex < lastIndex) {
+	                int nextIndex = currentIndex + 1;
+	                String nextId = getNavigationId(session, queryBase, moduleIDName, moduleLMD, nextIndex, jsonParser, "NextAreaId");
+	                if (!nextId.equals(moduleIDValue)) {
+	                    result[1] = String.valueOf(nextIndex);
+	                    result[2] = nextId;
+	                } else {
+	                    result[2] = moduleIDValue;
+	                }
+	            } else {
+	                result[2] = moduleIDValue;
+	            }
+	            break;
+
+	        case "0": // Previous
+	            if (currentIndex > 1) {
+	                int prevIndex = currentIndex - 1;
+	                String prevId = getNavigationId(session, queryBase, moduleIDName, moduleLMD, prevIndex, jsonParser, "PrevAreaId");
+	                if (!prevId.equals(moduleIDValue)) {
+	                    result[1] = String.valueOf(prevIndex);
+	                    result[2] = prevId;
+	                } else {
+	                    result[2] = moduleIDValue;
+	                }
+	            } else {
+	                result[2] = moduleIDValue;
+	            }
+	            break;
+
+	        case "3": // First
+	            result[1] = "1";
+	            result[2] = getNavigationId(session, queryBase, moduleIDName, moduleLMD, 1, jsonParser, "FirstID");
+	            break;
+
+	        case "4": // Last
+	            result[1] = String.valueOf(lastIndex);
+	            result[2] = getNavigationId(session, queryBase, moduleIDName, moduleLMD, lastIndex, jsonParser, "LastID");
+	            break;
+
+	        default: // Same/Current
+	            result[2] = moduleIDValue;
+	            break;
+	    }
+
+	    return result;
+	}
+
+	private String getNavigationId(Session session, String queryBase, String moduleIDName, String moduleLMD, int rowNum, JSONParser jsonParser, String label) throws ParseException {
+	    String navQuery =
+	            "SELECT json_object('" + label + "' VALUE (SELECT " + moduleIDName +
+	            " FROM (SELECT " + moduleIDName + ", ROW_NUMBER() OVER (ORDER BY " + moduleLMD + " DESC, " + moduleIDName + " DESC) AS row_num FROM (" + queryBase + ")) " +
+	            "WHERE row_num = " + rowNum + ")) FROM dual";
+
+	    Query q = session.createNativeQuery(navQuery);
+	    JSONObject rowNav = (JSONObject) jsonParser.parse(q.getSingleResult().toString());
+	    return rowNav.get(label).toString();
+	}
+
+
+	
+	
+	
+	
+	
 	///////////////////////////////////////node nav///////////////////////////////////////
 	//@SuppressWarnings("unchecked")
 		@SuppressWarnings("rawtypes")
