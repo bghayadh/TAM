d9aaa4ebef4b588160aa00faa6a8f773836e4bf8
Bilal Ghayad <bilmar_gh@yahoo.com>
2025-11-21 02:47:51 +0200

    Optimization.

--- src/main/java/com/aliat/alm/proc/SnglCmCntrl.java
+++ src/main/java/com/aliat/alm/proc/SnglCmCntrl.java
@@ -9,8 +9,6 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
-
 import org.apache.commons.lang3.StringUtils;
 import org.hibernate.Session;
 import org.slf4j.Logger;
